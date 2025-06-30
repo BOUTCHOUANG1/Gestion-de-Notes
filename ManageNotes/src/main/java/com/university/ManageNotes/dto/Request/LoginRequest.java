7011931f689086962d3d815b450f64959c73d417
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-06-30 12:32:15 +0100

    update loginRequest with constructor

--- ManageNotes/src/main/java/com/university/ManageNotes/dto/Request/LoginRequest.java
+++ ManageNotes/src/main/java/com/university/ManageNotes/dto/Request/LoginRequest.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
