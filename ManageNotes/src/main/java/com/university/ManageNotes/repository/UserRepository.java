88820001749b29026b60105bed79d25f49ce874b
Boutchouang Nathan <boutchouangelija@gmail.com>
2025-06-30 12:23:22 +0100

    update the userRepository

--- ManageNotes/src/main/java/com/university/ManageNotes/repository/UserRepository.java
+++ ManageNotes/src/main/java/com/university/ManageNotes/repository/UserRepository.java
@@ -15,7 +15,11 @@ public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email);
 
+    Optional<Users> findByUsername(String username);
+
     boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 
     List<Users> findByRole(Role role);
 
